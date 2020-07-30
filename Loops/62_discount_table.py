268b55868e1ecad8140a5e4ba4071ebd8b22eb52
guumee <guzelya93@gmail.com>
2020-07-31 02:50:38 +0300

    Done task

--- Loops/62_discount_table.py
+++ Loops/62_discount_table.py
@@ -2,10 +2,23 @@
 A particular retailer is having a 60 percent off sale on a variety of discontinued
 products. The retailer would like to help its customers determine the reduced price
 of the merchandise by having a printed discount table on the shelf that shows the
-original prices and the prices after the discount has been applied. Write a program that
-uses a loop to generate this table, showing the original price, the discount amount,
-and the new price for purchases of $4.95, $9.95, $14.95, $19.95 and $24.95. Ensure
-that the discount amounts and the new prices are rounded to 2 decimal places when
-they are displayed.
-
+original prices and the prices after the discount has been applied.
+Write a program that uses a loop to generate this table, showing the original price,
+the discount amount, and the new price for purchases of $4.95, $9.95, $14.95,
+$19.95 and $24.95. Ensure that the discount amounts and the new prices are rounded
+to 2 decimal places when they are displayed.
 """
+
+SALE = 0.6
+
+prices = [4.95, 9.95, 14.95, 19.95, 24.95]
+
+i = 0
+print(" "*5," | ORIGINAL PRICE | DISCOUNT | NEW PRICE |")
+while i <= len(prices):
+    discount = prices[i] * SALE
+    new_price = prices[i] - discount
+    print(" "*5, " | ", " "*7, prices[i], " | ", " "*1, " %.2f | " % discount,  " "*1, " %.2f" % new_price, " |")
+    i = i+1
+
+
